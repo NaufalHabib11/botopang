cea8dfe78997c1afdc14c4a8f2f063561541f87d
NaufalHabib11 <53120857+NaufalHabib11@users.noreply.github.com>
2021-12-30 19:26:41 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const model = require('./sdk/model.js'); //predict
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '5057562264:AAHLUz5s6G-8vDMtapgD6l1Y4Z2i5su7OlM'
+const token = '5064002940:AAEQGMP6n0taVboAXvlrwD3zng13ywKhzA8'
 const bot = new TelegramBot(token, {polling: true});
 
 state=0;
